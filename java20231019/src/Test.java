f7edbe999228042fea871a2f8357b75c8e4ade39
seanycy <145823999+seanycy@users.noreply.github.com>
2023-10-19 13:36:48 +0800

    今日课堂代码

---
+++ java20231019/src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Thursday!!!!");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
